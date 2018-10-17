62dafb1237923b85d0080877a441bfe608e37407
Jason Eberle <jason@imgix.com>
2018-10-17 18:58:57 -0400

    Remove stray console.log

--- tools/pipelines/pipeline.datastore-compute-all.js
+++ tools/pipelines/pipeline.datastore-compute-all.js
@@ -4,8 +4,6 @@ var _ = require('lodash'),
 module.exports = function setupDatastoreComputeAllPipeline(gulp) {
   return function datastoreComputeAllPipeline() {
     return through.obj(null, null, function flush(callback) {
-      console.log('flush');
-
       // Refresh all
       _.chain(gulp.ds.definitions)
         .flatMap(function getAllItems(def) {
